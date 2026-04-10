d1c58237297139694fe3fdcdbe18ea949fac1d72
NIHAL  T P <nihaltpnki@gmail.com>
2026-04-10 22:13:28 +0530

    feat: Enable resource shrinking and code minification for release build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = false
-            isShrinkResources = false
+                isMinifyEnabled = true
+                isShrinkResources = true
         }
     }
 }
