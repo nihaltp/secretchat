710f5d116927133ce0142c17d867e7a0d83c60c5
NIHAL  T P <nihaltpnki@gmail.com>
2026-04-14 01:29:13 +0530

    feat(branding): add debug logo asset and update AppLogoTitle to use it

--- lib/widgets/app_logo_title.dart
+++ lib/widgets/app_logo_title.dart
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2026 Secret Chat Contributors
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class AppLogoTitle extends StatelessWidget {
@@ -11,12 +12,16 @@ class AppLogoTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final String logoAssetPath = kDebugMode
+        ? 'assets/branding/app_logo_debug.png'
+        : 'assets/branding/app_logo.jpg';
+
     return Row(
       children: [
         ClipRRect(
           borderRadius: BorderRadius.circular(6),
           child: Image.asset(
-            'assets/branding/app_logo.jpg',
+            logoAssetPath,
             width: logoSize,
             height: logoSize,
             fit: BoxFit.cover,
